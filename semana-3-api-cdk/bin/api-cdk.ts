08e4fe0e4c1f3ef7062da7b8d4666226eb2edf8d
Roddy Correa Tenesaca <roddy.correa@kushkipagos.com>
2024-05-16 11:40:59 -0500

    add new variables

--- semana-3-api-cdk/bin/api-cdk.ts
+++ semana-3-api-cdk/bin/api-cdk.ts
@@ -7,7 +7,6 @@ const app = new cdk.App();
 new HelloWorldLambdaStack(app, 'HelloWorldLambdaStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
+    region: process.env.CDK_DEFAULT_REGION ?? 'us-east-1',
   }
-
 });
